fba49e9e9fdbf8c156501c8ee717c22ad864d866
yonggyo1125 <yonggyo00@kakao.com>
2023-10-20 17:59:14 +0900

    의존성 추가 설정(pom.xml), 톰캣10 서버 설정 완료

--- src/test/java/models/member/JoinService.java
+++ src/test/java/models/member/JoinService.java
@@ -2,6 +2,9 @@ package models.member;
 
 import commons.BadRequestException;
 import commons.Validator;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Objects;
 
 public class JoinService {
     private Validator validator;
@@ -17,5 +20,20 @@ public class JoinService {
         validator.check(member);
 
         memberDao.register(member);
+    }
+
+    public void join(HttpServletRequest request) {
+        String _agree = Objects.requireNonNullElse(request.getParameter("agree"), "false");
+        boolean agree = _agree.equals("true") ? true : false;
+
+        Member member = Member.builder()
+                .userId(request.getParameter("userId"))
+                .userPw(request.getParameter("userPw"))
+                .confirmUserPw(request.getParameter("confirmUserPw"))
+                .email(request.getParameter("email"))
+                .userNm(request.getParameter("userNm"))
+                .agree(agree)
+                .build();
+        join(member);
     }
 }
