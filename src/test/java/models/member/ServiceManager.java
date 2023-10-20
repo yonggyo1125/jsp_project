cbe686edaaf2fff4804535528ceca9623c195820
yonggyo1125 <yonggyo00@kakao.com>
2023-10-20 15:53:41 +0900

    의존성 추가 설정(pom.xml), 톰캣10 서버 설정 완료

--- src/test/java/models/member/ServiceManager.java
+++ src/test/java/models/member/ServiceManager.java
@@ -19,7 +19,10 @@ public class ServiceManager {
     }
 
     public JoinValidator joinValidator() {
-        return new JoinValidator();
+        JoinValidator validator = new JoinValidator();
+        validator.setMemberDao(memberDao());
+
+        return validator;
     }
 
     public JoinService joinService() {
