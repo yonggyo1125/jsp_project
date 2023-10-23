8d39b7677dc42cf234c4fe71ed3a63115963f07a
yonggyo1125 <yonggyo00@kakao.com>
2023-10-23 14:18:48 +0900

    기능통합 - 회원가입

--- src/main/java/controllers/member/JoinController.java
+++ src/main/java/controllers/member/JoinController.java
@@ -1,10 +1,13 @@
 package controllers.member;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.member.JoinService;
+import models.member.ServiceManager;
 
 import java.io.IOException;
 
@@ -13,10 +16,15 @@ public class JoinController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/templates/member/join.jsp");
+        rd.forward(req, resp);
     }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        JoinService service = ServiceManager.getInstance().joinService();
+        service.join(req);
 
     }
 }
